c2d2a96cb7b0679085402ebb04b5959a1f066311
mbui <Minhtri.bui@md.de>
2021-01-28 07:54:56 +0100

    unused logging deleted

--- __tests__/global-setup.js
+++ __tests__/global-setup.js
@@ -1,16 +1,11 @@
 const io = require('@actions/io')
 const path = require('path')
 module.exports = async () => {
-  console.log('setup test')
   const toolDir = path.join(__dirname, 'runner', 'tools')
   const tempDir = path.join(__dirname, 'runner', 'temp')
 
   process.env['RUNNER_TOOL_CACHE'] = toolDir
-
-  process.env['RUNNER_TEMP'] = tempDir
-  
-  console.log(`clear tooldir:${toolDir} and tempDir: ${tempDir}`)
-
+  process.env['RUNNER_TEMP'] = tempDir  
   await io.rmRF(toolDir)
   await io.rmRF(tempDir)
 }
--- __tests__/global-teardown.js
+++ __tests__/global-teardown.js
@@ -2,7 +2,5 @@ const io = require('@actions/io')
 
 module.exports = async () => {
   const toolDir = process.env['RUNNER_TOOL_CACHE']
-  const tempDir = process.env['RUNNER_TEMP']
-  console.log('tear down')
   await io.rmRF(toolDir)
 };
